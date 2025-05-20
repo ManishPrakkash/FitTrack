4b5be690108a93d33f71c6d6e42c5f84c0ff013a
ManishPrakkash <manishprakkash@gmail.com>
2025-05-20 15:12:15 +0530

    backend test files removed

--- backend/run_with_mongodb.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Django server with MongoDB settings...
-set DJANGO_SETTINGS_MODULE=fittrack_backend.settings_mongodb
-python manage.py runserver
--- backend/run_with_mongodb_verbose.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Django server with MongoDB settings in verbose mode...
-set DJANGO_SETTINGS_MODULE=fittrack_backend.settings_mongodb
-python manage.py runserver --verbosity 3
